b4b60174e6a13f43dd690d8b55f683ea4e57f8c5
“EmilieFischer” <emil609u@stud.kea.dk>
2022-02-24 15:50:48 +0100

    arbejde indtil burgermenu

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// const burgerikon = document.querySelector("nav .burger");
+// const burgercontent = document.querySelector("nav .burgercontent");
+
+// burgercontent.addEventListener("click", fjernBurger);
+
+// function fjernBurger() {
+//   burgerikon.classList.toggle("left-0");
+//   burgerikon.classList.toggle("left-full");
+
+//   burgercontent.classList.toggle("left-0");
+//   burgercontent.classList.toggle("left-full");
+// }
